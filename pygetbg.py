2d4a41b352f31a1f76c746f88d61b1865c40257b
manoon <neoo0ooen@gmail.com>
2014-06-03 07:51:39 +0800

    a fork test

--- pygetbg.py
+++ pygetbg.py
@@ -1,12 +1,14 @@
-mport re
+import re
 import urllib
+print "Usage pygetbg.py /path/to/remote/imgages/dir"
 f2 = open("./style.css", "r")
 str2=f2.read()
 print str2
 print "*"*10
 pattern = re.compile(r'.jpg')
 imglist = re.findall(r'url\(..\/images\/(.*).gif',str2)
-imgdir="http://www.tsinghua.edu.cn/publish/th/images/"
+#imgdir="http://www.tsinghua.edu.cn/publish/th/images/"
+imgdir=sys.argv[1]
 print imgdir
 print imglist
 urllist=[]
